22668cef1e50d073353c9efee08390fbd7a93575
System <none>
2017-03-26 22:08:43 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var form = {
   redirect_uri:'https://understood-bird.glitch.me/reddit',
 }
 
-// var url = form.redirect_uri
-var url = 'https://www.reddit.com/api/v1/access_token'
-var auth = {user: process.env.CLIENT_ID, pass: process.env.CLIENT_SECRET, 'sendImmediately': false}
+var url = form.redirect_uri
+// var url = 'https://www.reddit.com/api/v1/access_token'
+var auth = {user: process.env.CLIENT_ID, pass: process.env.CLIENT_SECRET}
 /*
 const r = new snoowrap({
   userAgent: 'snoowrap-test',
@@ -28,14 +28,15 @@ app.use((req, res, next) =>{
 })
 
 app.get('/', (req, res) => {
-  res.send('<title>Test Page</title><body>Nothing to see here</body>');
+  //res.send('<title>Test Page</title><body>Nothing to see here</body>');
   request.post({url: url, form: form, auth: auth}), function(err, res, body) {
-    console.log(res)
+    res.send(body)
+    return
   }
 });
 
 app.use('/reddit', bodyParser.json(), (req, res) => {
-  res.json({body: 'yes'})
+  res.json({body: req.body})
 })
 
 app.get('/about', (req, res) => {
@@ -46,3 +47,36 @@ app.get('/about', (req, res) => {
 var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+/*
+request({
+    // will be ignored 
+    method: 'POST',
+    uri: 'http://www.google.com',
+ 
+    // HTTP Archive Request Object 
+    har: {
+      url: 'http://www.mockbin.com/har',
+      method: 'POST',
+      headers: [
+        {
+          name: 'content-type',
+          value: 'application/x-www-form-urlencoded'
+        }
+      ],
+      postData: {
+        mimeType: 'application/x-www-form-urlencoded',
+        params: [
+          {
+            name: 'foo',
+            value: 'bar'
+          },
+          {
+            name: 'hello',
+            value: 'world'
+          }
+        ]
+      }
+    }
+  })
+*/
