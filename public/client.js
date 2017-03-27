73cf7ada9cab43d411a17d0e827e5db02b015396
System <none>
2017-03-27 20:23:47 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -3,7 +3,29 @@ angular.module('reddit', [])
 .service('r', function($http) {
   return {
     subReddit: function(sub) {
-      $http.get('/r/' + sub).then
+      $http.get('/r/' + sub).then(
+        function(res) {
+          console.log(res)
+          return {data: res.data}
+        }, function(res) {
+          return {error: res}
+        }
+      )
+    }
+  }
+})
+
+.directive('rSub', function(r) {
+  return {
+    restrict: 'A', 
+    compile: function() {
+      return {
+        pre: function(scope, element, attribs) {
+          element.bind('click', function(e) {
+            r.subReddit(attribs.rSub)
+          })
+        }
+      }
     }
   }
 })
