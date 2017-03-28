3804675dc317dfd854d101a8e9945cdafea73af4
System <none>
2017-03-28 13:27:08 +0000

    Checkpoint

--- reddit.js
+++ reddit.js
@@ -1,35 +1,32 @@
 var request = require('request')
 var _accessToken = null
 
-var _newAccessToken = (cb) => {
-  var options = {
-    url: 'https://www.reddit.com/api/v1/access_token', 
-    method: 'POST',
-    form: {grant_type: 'client_credentials'},
-    auth: {user: process.env.CLIENT_ID, pass: process.env.CLIENT_SECRET}
-  }
-  
+var _newAccessToken = () => {
   return new Promise((resolve, reject) => {
-    
-  })
+    var options = {
+      url: 'https://www.reddit.com/api/v1/access_token', 
+      method: 'POST',
+      form: {grant_type: 'client_credentials'},
+      auth: {user: process.env.CLIENT_ID, pass: process.env.CLIENT_SECRET}
+    }
+    request(options, function(err, response, body) {
+      var oBody = JSON.parse(body)
+      setTimeout(() => {
+        _accessToken = null
+      }, (Number(oBody.expires_in) * 1000) - 60000)
 
-  request(options, function(err, response, body) {
-    var oBody = JSON.parse(body)
-    setTimeout(() => {
-      _accessToken = null
-    }, (Number(oBody.expires_in) * 1000) - 60000)
-    
-    _accessToken = oBody.access_token
+      _accessToken = oBody.access_token
 
-    if (cb) cb(_accessToken)
+      resolve(_accessToken)
+    })
   })
 }
 
-var access_token =(cb) => {
+var access_token = (cb) => {
   if (!_accessToken) {
     // Request a new token from Reddit, the old one expired or will do in a minute
-    _newAccessToken((token)=>{
-      cb(_accessToken)
+    _newAccessToken().then((token) => {
+      cb(token)
     })
   } else {
     // Token already requested from Reddit, still valid
